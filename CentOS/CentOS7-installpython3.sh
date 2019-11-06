1856054d16b4c7328fd98256dd41b08fcf525732
sqtest <sqtest@sqtest.com>
2019-11-07 00:34:55 +0800

    update tabao mirrors

--- CentOS/CentOS7-installpython3.sh
+++ CentOS/CentOS7-installpython3.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 yum groupinstall -y "Development tools"
 yum install -y sqlite-devel ncurses-devel ncurses-libs zlib-devel mysql-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel openssl-devel
-wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tar.xz && xz -d Python-3.6.9.tar.xz && tar xvf Python-3.6.9.tar
+wget  https://npm.taobao.org/mirrors/python/3.6.9/Python-3.6.9.tar.xz && xz -d Python-3.6.9.tar.xz && tar xvf Python-3.6.9.tar
 cd Python-3.6.9
 ./configure
 make&& make install
 cd ..
-rm -rf Python-3.6.9 Python-3.6.9.tar.xz
+rm -rf Python-3.6.9 Python-3.6.9.tar
 
 
 #
